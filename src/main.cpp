628423d2cb7e95d5ff5e50638774212714f7c1a8
Malte Schilling <schilling.malte@googlemail.com>
2025-08-04 00:14:00 +0200

    cleared code, ready for rewrite

--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,9 @@
 #include <csignal>
 #include <atomic>
 
-#include "structs.hpp"
-#include "functions.hpp"
+#include "my_atomics.hpp"
 
 int main(int argc, char** argv){
 	std::signal(SIGINT, [](int){stop_flag = true;});
 
-	using namespace localOverloads;
-	using namespace localFunctions;
-	using clock = std::chrono::steady_clock;
-
-	// initialize starting point
-	clock::time_point start = clock::now();
-
-	switch (argc){
-		case 1:
-			std::cout << "0:00:00.000\n" <<
-			/*         */"Return = Set checkpoint\n" <<
-			/*         */"CTRL+C = Stop stopwatch" << std::flush;
-
-			//go back to the first line and start the stopwatch displaying cycle.
-			std::cout << ANSI::up_line << ANSI::up_line << std::flush;
-			stopwatch(start);
-
-			std::cout << "\n\n" << ANSI::bold << "Stopped stopwatch at:\n" << stopwatch(start) << ANSI::reset << std::endl;
-
-			return 0;
-
-		default:
-			std::cout << ANSI::bold << "Usage:" << ANSI::reset <<" \"" <<
-			/*         */*argv << "\"\n" << ANSI::bold << "Note: " << ANSI::reset <<
-			/*         */"Only works with terminals, that support ANSI-Sequences\n" << std::endl;
-
-			return 1;
-	}
 }
---
+++ src/my_atomics.hpp
@@ -0,0 +1,8 @@
+#ifndef MY_ATOMICS_HPP
+#define MY_ATOMICS_HPP
+#include <atomic>
+
+std::atomic<bool> stop_flag = false;
+
+
+#endif
