17d76bde3b2932d9c3080613bd76ec0c6fe88618
Malte Schilling <schilling.malte@googlemail.com>
2025-09-06 23:51:18 +0200

    put unused function to the back, added variant of make_output

--- src/stopwatch.hpp
+++ src/stopwatch.hpp
@@ -33,15 +33,17 @@ namespace stopwatch{
 	/// @return hours:mins:seconds.milliseconds, 2 digits for minutes and seconds, 3 digits for milliseconds
 	std::string make_output(clock::time_point start);
 
-	//UNUSED
-/* 	/// @brief Restores std::chrono::steady_clock::time_point from a std::string
-	/// @param output the string, which holds the time_point (format: "[0-9]+:[0-9]{2}:[0-9]{2}.[0-9]{3}" order: "h min sec ms")
-	/// @return Timepoint
-	clock::time_point get_input(std::string_view output); */
+	std::string make_output(const checkpoints_t& checkpoints);
 
 	/// @brief adds a checkpoint to the vector
 	/// @param checkpoints the checkpoints object, in which the checkpoint should be added
 	/// @param start Timepoint of begin
 	void trig_checkpoint(checkpoints_t &checkpoints, clock::time_point start);
+
+	//UNUSED
+/* 	/// @brief Restores std::chrono::steady_clock::time_point from a std::string
+	/// @param output the string, which holds the time_point (format: "[0-9]+:[0-9]{2}:[0-9]{2}.[0-9]{3}" order: "h min sec ms")
+	/// @return Timepoint
+	clock::time_point get_input(std::string_view output); */
 }
 #endif
